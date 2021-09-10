05ef2f7531bfc740bef3e6e88393402e248f5443
Mahak Agarwal <65973339+missmahi55@users.noreply.github.com>
2021-09-10 22:52:34 +0530

    tavis update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(morgan("dev"));
 connectDB();
-
+console.log("tryyyy");
+//define routes and api
 app.use(express.json({extended: false}));
 
 app.use("/api/users", require("./routes/userAPI"));
