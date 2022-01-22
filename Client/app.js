b86202edcbe72a67c829463902e93e56251e1833
missmahi55 <mahakagarwal248@gmail.com>
2022-01-22 20:09:20 +0530

    First commit

--- Client/app.js
+++ Client/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-const server_host = process.env.YOUR_HOST || '0.0.0.0';
+//const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+//const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, "/build")));
 
@@ -11,4 +11,4 @@ app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-app.listen(server_port, server_host, (_)=>console.log("`React app is listening `"));
+//app.listen(server_port, server_host, (_)=>console.log("`React app is listening `"));
--- Client/dist/app.dev.js
+++ Client/dist/app.dev.js
@@ -4,14 +4,11 @@ var express = require("express");
 
 var app = express();
 
-var path = require("path");
+var path = require("path"); //const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+//const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
 app.use(express["static"](path.join(__dirname, "/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/build/index.html"));
-});
-app.listen(server_port, server_host, function (_) {
-  return console.log("`React app is listening `");
-});
+}); //app.listen(server_port, server_host, (_)=>console.log("`React app is listening `"));
