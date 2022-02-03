c348a596cf07a34cf596d31ceb4e08b5cc9d3b8e
missmahi55 <mahakagarwal248@gmail.com>
2022-02-03 21:04:36 +0530

    first commit

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -15,6 +15,10 @@ require("dotenv").config();
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+var path = require('path');
+
+app.use(express["static"](path.join(__dirname, "Client/build")));
 app.use(cors());
 app.use(morgan("dev"));
 connectDB(); //define routes and api
@@ -28,8 +32,8 @@ app.use("/api/auth", require("./routes/authAPI"));
 app.use("/api/profile", require("./routes/ProfileAPI"));
 app.use("/api/cart", require("./routes/cartAPI"));
 app.use("/api/payment", require("./routes/PaymentAPI"));
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '/Client/build/index.html')); // relative path
+app.get("/", function (req, res) {
+  res.send("App is up");
 });
 app.listen(server_port, server_host, function () {
   console.log("Server is listening ");
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
-
+const path = require('path');
+app.use(express.static(path.join(__dirname, "Client/build")))
 app.use(cors());
 app.use(morgan("dev"));
 connectDB();
@@ -22,9 +23,9 @@ app.use("/api/profile", require("./routes/ProfileAPI"));
 app.use("/api/cart", require("./routes/cartAPI"));
 app.use("/api/payment", require("./routes/PaymentAPI"));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/Client/build/index.html')) // relative path
-  })
+app.get("/", (req, res)=>{
+    res.send("App is up");
+});
 
 app.listen(server_port, server_host, ()=>{
     console.log(`Server is listening `)
