58b45479608d9cb3a906a1fe216a3d13b9933f6f
missmahi55 <mahakagarwal248@gmail.com>
2022-03-17 22:31:55 +0530

    updations

---
+++ Client/src/util/dist/index.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.decodeUser = exports.getServer = void 0;
+
+var _jwtDecode = _interopRequireDefault(require("jwt-decode"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//import axios from "axios";
+var isDevelopment = window.location.hostname.includes("localhost");
+
+var getServer = function getServer() {
+  return isDevelopment ? "http://localhost:5000" : "";
+};
+
+exports.getServer = getServer;
+
+var decodeUser = function decodeUser() {
+  var token = localStorage.getItem("token");
+  return (0, _jwtDecode["default"])(token);
+};
+
+exports.decodeUser = decodeUser;
