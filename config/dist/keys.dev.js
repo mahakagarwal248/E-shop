b6787346b6459951855ee45fef18ca9e3ecf76f5
missmahi55 <mahakagarwal248@gmail.com>
2022-03-17 22:29:02 +0530

    updations

--- config/dist/keys.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-  mongoURI: "mongodb+srv://mahakagarwal:Mylife23@productscollection.uachd.mongodb.net/ecommerceapp?retryWrites=true&w=majority",
-  jwtSecret: "ecommerceSecret"
-};
