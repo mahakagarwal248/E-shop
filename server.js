5761ca1cb416ac59ee5a56a86e9a0723e77db300
missmahi55 <mahakagarwal248@gmail.com>
2022-03-17 22:19:37 +0530

    updations

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 //const PORT = process.env.PORT || 5000;
 require("dotenv").config();
-const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-const server_host = process.env.YOUR_HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
 const path = require('path');
-app.use(express.static(path.join(__dirname, "Client/build")))
+
 app.use(cors());
 app.use(morgan("dev"));
 connectDB();
@@ -23,10 +22,13 @@ app.use("/api/profile", require("./routes/ProfileAPI"));
 app.use("/api/cart", require("./routes/cartAPI"));
 app.use("/api/payment", require("./routes/PaymentAPI"));
 
-app.get("/", (req, res)=>{
-    res.send("App is up");
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("Client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
+    });
+  }
 
-app.listen(server_port, server_host, ()=>{
+app.listen(port, ()=>{
     console.log(`Server is listening `)
 });
